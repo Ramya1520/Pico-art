b6f8ded47bc7207b4f669b31fcfcd150806caf0f
ramya1520 <ramya.krishnan1520@gmail.com>
2023-06-01 20:53:24 +0530

    add-footer

--- src/component/Room.js
+++ src/component/Room.js
@@ -1,23 +1,21 @@
-import React, { useEffect, useRef, useState } from "react";
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import React, {useRef, useState } from "react";
+import { Canvas, useFrame} from "@react-three/fiber";
 import "../App.css";
 import { SoftShadows, CameraControls, useGLTF } from "@react-three/drei";
-import { easing } from "maath";
-import InputColor from "react-input-color";
-import { Button, Container, Row, TabPane } from "reactstrap";
-import { GLTF } from "@react-three/drei";
+import { Button, Container, Row} from "reactstrap";
 import { useLoader } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 function Room() {
   const { materials } = useGLTF("/silvania_e_adilson.glb");
   const [show2DView, setShow2DView] = useState(false);
-  const [color, setColor] = useState({});
   const [width, setWidth] = useState(5);
   const [height, setHeight] = useState(5);
   const [depth, setDepth] = useState(5);
   const [show3Dimage, setShow3Dimage] = useState([]);
   const [thickness, setThickness] = useState(0.2);
+  const meshRef = useRef();
+
   let table_sofa = useLoader(GLTFLoader, "/table_sofa.glb");
   let table = useLoader(GLTFLoader, "/modern_dining_table.glb");
   let showcase = useLoader(GLTFLoader, "/showcase.glb");
@@ -28,24 +26,17 @@ function Room() {
   let showcase_1 = useLoader(GLTFLoader, "/showcase_1.glb");
   let television_1 = useLoader(GLTFLoader, "/tv_stand_1.glb");
   let cup_1 = useLoader(GLTFLoader, "/cup_and_plate_1.glb");
-  const items = [table_sofa, table, showcase, cup, television];
-  const duplicate_items = [
-    table_sofa_1,
-    table_1,
-    showcase_1,
-    cup_1,
-    television_1,
-  ];
 
-  const meshRef = useRef();
+  const items = [table_sofa, table, showcase, cup, television];
+  const duplicate_items = [table_sofa_1,table_1,showcase_1,cup_1,television_1,];
 
   const Furnitures = () => {
     const tablePositions = [
+      { x: 0, y: 3, z: 0 },
       { x: 0, y: 2, z: 0 },
       { x: 0, y: 1, z: 0 },
-      { x: 0, y: 0, z: 0 },
-      { x: 0, y: -0.5, z: 0 },
-      { x: 0, y: -2, z: 0 },
+      { x: 0, y: 0.5, z: 0 },
+      { x: 0, y: -1, z: 0 },
     ];
     const tableScales = [
       { x: 0.0004, y: 0.0004, z: 0.0004 },
@@ -135,34 +126,6 @@ function Room() {
     );
   };
 
-  function Light() {
-    const ref = useRef();
-    useFrame((state, delta) => {
-      easing.dampE(
-        ref.current.rotation,
-        [(state.pointer.y * Math.PI) / 50, (state.pointer.x * Math.PI) / 20, 0],
-        0.2,
-        delta
-      );
-    });
-    return (
-      <group ref={ref}>
-        <directionalLight
-          position={[5, 5, -8]}
-          castShadow
-          intensity={5}
-          shadow-mapSize={2048}
-          shadow-bias={-0.001}
-        >
-          <orthographicCamera
-            attach="shadow-camera"
-            args={[-8.5, 8.5, 8.5, -8.5, 0.1, 20]}
-          />
-        </directionalLight>
-      </group>
-    );
-  }
-
   function Box(props) {
     const mesh = useRef();
     return (
@@ -172,41 +135,26 @@ function Room() {
     );
   }
 
-function TwoD({ width, height, depth, thickness, materials }) {
-  const groupRef = useRef();
+  function TwoD({ width,depth, thickness, materials }) {
+    const groupRef = useRef();
 
-  useFrame(({ mouse }) => {
-    const { x,y } = mouse;
-    const rotationY = (x - 0.5) * Math.PI * 2; 
-    const rotationX = (y - 0.5) * Math.PI * 2; 
-    groupRef.current.rotation.set(0,rotationY,0);
-  });
+    useFrame(({ mouse }) => {
+      const { x, y } = mouse;
+      const rotationY = (x - 0.5) * Math.PI * 2;
+      
+      groupRef.current.rotation.set(0, rotationY, 0);
+    });
 
-  return (
-    <group ref={groupRef} dispose={null}>
-      <Box
-        scale={[thickness, height, depth]}
-        material={materials.Material_2146804362}
-        position={[-1 * (width / 2), height / 2, 0]}
-      />
-      <Box
-        scale={[width, height, thickness]}
-        material={materials.Material_2146804362}
-        position={[0, height / 2, -1 * (depth / 2)]}
-      />
-      <Box
-        scale={[thickness, height, depth]}
-        material={materials.Material_2146804362}
-        position={[width / 2, height / 2, 0]}
-      />
-      <Box
-        scale={[width, thickness, depth]}
-        material={materials.Material_2146804362}
-        position={[0, 0, 0]}
-      />
-    </group>
-  );
-}
+    return (
+      <group ref={groupRef} dispose={null}>
+        <Box
+          scale={[width, thickness, depth]}
+          material={materials.Material_2146804362}
+          position={[0, 0, 0]}
+        />
+      </group>
+    );
+  }
 
   function ThreeD({ width, height, depth, thickness, materials }) {
     return (
@@ -237,40 +185,62 @@ function TwoD({ width, height, depth, thickness, materials }) {
   const toggleView = () => {
     setShow2DView((prev) => !prev);
   };
-  const handleColorChange = (newColor) => {
-    setColor(newColor.hex);
+
+  const onChangeWidth = (event) => {
+    setWidth(event.target.value);
   };
-  const onChangeWidth = (newWidth) => {
-    setWidth(parseInt(newWidth.target.value));
+  const onChangeHeight = (event) => {
+    setHeight(event.target.value);
   };
-  const onChangeHeight = (newHeight) => {
-    setHeight(parseInt(newHeight.target.value));
+  const onChangeDepth = (event) => {
+    setDepth(event.target.value);
   };
-  const onChangeDepth = (newDepth) => {
-    setDepth(parseInt(newDepth.target.value));
+  const onChangethickness = (event) => {
+    setThickness(event.target.value);
   };
-  const onChangeThickness = (newThickness) => {
-    setThickness(parseInt(newThickness.target.value));
+
+  const Footer = () => {
+    return (
+      <React.Fragment>
+        <footer className="footer">
+          <span>Height:</span>
+          <input
+            type="text"
+            placeholder="Height(cm)"
+            onChange={(e) => onChangeHeight(e)}
+            value={height}
+          />
+          <span>Width:</span>
+          <input
+            type="text"
+            placeholder="Width(cm)"
+            onChange={(e) => onChangeWidth(e)}
+            value={width}
+          />
+          <span>Depth:</span>
+          <input
+            type="text"
+            placeholder="Depth(cm)"
+            onChange={(e) => onChangeDepth(e)}
+            value={depth}
+          />
+          <span>Wall Thickness:</span>
+          <input
+            type="text"
+            value={thickness}
+            onChange={(e) => onChangethickness(e)}
+          />
+
+          <Button onClick={toggleView}>
+            {show2DView ? "Switch to 3D" : "Switch to 2D"}
+          </Button>
+        </footer>
+      </React.Fragment>
+    );
   };
+
   return (
     <div>
-      <button
-        onClick={toggleView}
-        style={{
-          position: "absolute",
-          top: "10px",
-          left: "10px",
-          zIndex: 9999,
-          padding: "10px",
-          backgroundColor: "transparent",
-          color: "#000",
-          border: "1px solid #000",
-          borderRadius: "4px",
-          cursor: "pointer",
-        }}
-      >
-        {show2DView ? "Switch to 3D" : "Switch to 2D"}
-      </button>
       <Container fluid>
         <Row>
           <div className="col-lg-1">
@@ -282,69 +252,23 @@ function TwoD({ width, height, depth, thickness, materials }) {
               <SoftShadows resolution={1024} />
             </Canvas>
           </div>
-{/* 
-          <div className="col-lg-4">
-            <div className="login-page">
-              <div className="form">
-                <form className="login-form">
-                  <div className="property">
-                    <input
-                      type="text"
-                      placeholder="Height(cm)"
-                      onChange={onChangeHeight}
-                    />
-                    <input
-                      type="text"
-                      placeholder="Width(cm)"
-                      onChange={onChangeWidth}
-                    />
-                    <input
-                      type="text"
-                      placeholder="Depth(cm)"
-                      onChange={onChangeDepth}
-                    />
-                  </div>
-                  <div>
-                    <h6 className="display_flex">Color picker</h6>
-                    <div className="property">
-                      <InputColor
-                        initialValue="#5E72E4"
-                        onChange={handleColorChange}
-                        placement="left"
-                      />
-                    </div>
-                    <input
-                      type="text"
-                      placeholder="Wall Thickness(cm)"
-                      onChange={onChangeThickness}
-                    />
-                  </div>
-                  <div className="btns property">
-                    <Button className="save btn-primary">Save</Button>{" "}
-                    <span> &nbsp;</span>
-                    <Button className="save btn-primary">Reset</Button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div> */}
-          <div className="col-lg-11">
-            <div id="canvas-container">
+          <div className="col-lg-10">
+            <div id="canvas-container" className="canvas-container">
               {show2DView ? (
-               <Canvas shadows camera={{ position: [0, 80, 0], fov: 10 }}>
-               <ambientLight intensity={0.5} />
-               <pointLight position={[10, 10, 10]} />
-               {/* <CameraControls /> */}
-               <SoftShadows resolution={12} />
-               <TwoD
-                 width={width}
-                 height={height}
-                 depth={depth}
-                 thickness={thickness}
-                 materials={materials}
-               />
-               <RoomFurniture />
-             </Canvas>
+                <Canvas shadows camera={{ position: [0, 80, 0], fov: 10 }}>
+                  <ambientLight intensity={0.5} />
+                  <pointLight position={[10, 10, 10]} />
+                  {/* <CameraControls /> */}
+                  <SoftShadows resolution={12} />
+                  <TwoD
+                    width={width}
+                    height={height}
+                    depth={depth}
+                    thickness={thickness}
+                    materials={materials}
+                  />
+                  <RoomFurniture />
+                </Canvas>
               ) : (
                 <Canvas shadows camera={{ position: [0, 80, 0], fov: 10 }}>
                   <ambientLight intensity={0.5} />
@@ -362,6 +286,7 @@ function TwoD({ width, height, depth, thickness, materials }) {
                 </Canvas>
               )}
             </div>
+            <Footer />
           </div>
         </Row>
       </Container>
