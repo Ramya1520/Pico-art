c8e3625e95192b6db30f126711929d565c4a6c7d
ramya1520 <ramya.krishnan1520@gmail.com>
2023-06-02 13:34:38 +0530

    footer change

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,24 @@
 import React from "react";
-import './Header.css'
-import { Navbar, Nav,Button ,Row} from "reactstrap";
+import "./Header.css";
+import { Navbar, Nav, Button, Row } from "reactstrap";
 import { useState } from "react";
 
 const Header = (title) => {
-
   return (
     <header className="pt-16 pb-16 header-background">
-        <div className="header-item">
-            <div>
-          <h3 className=''>Pico Art </h3>
-          </div>
-          <div className="logout">
+      <div className="header-item">
+        {/* <div> */}
+        <h3 className="">Pico Art </h3>
+        {/* </div> */}
+        {/* <div className="logout">
                   <Button variant="light" className="id">
                     User ID: EMP001
                   </Button>
                   <Button variant="light" className="logout">
                     Logout <img className="signout"></img>
                   </Button>
-                  </div>
-                  </div>
-              
+                  </div> */}
+      </div>
     </header>
   );
 };
